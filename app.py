ad90f53b33aa7f65fc2fc3ac96d58c59866d4f20
alfisyukri <alfis7433@gmail.com>
2024-04-19 09:30:51 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, jsonify
-import requests
-from bs4 import BeautifulSoup
+from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+from bs4 import BeautifulSoup
 from datetime import datetime
 
 import os
